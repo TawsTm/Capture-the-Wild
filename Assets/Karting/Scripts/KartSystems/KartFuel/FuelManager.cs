721c0848882e872c28226cf48cc57d9cf8a2de60
TawsTm <phil.wandel@hotmail.de>
2022-06-24 02:53:54 +0200

    Fix VideoManager bug where the video from the animal before was played

--- Assets/Karting/Scripts/KartSystems/KartFuel/FuelManager.cs
+++ Assets/Karting/Scripts/KartSystems/KartFuel/FuelManager.cs
@@ -10,7 +10,7 @@ public class FuelManager : MonoBehaviour
     private static float fuel = 100f;
     private static float currentFuel;
 
-    private float fuelBurnrate = 20f;
+    private float fuelBurnrate = 3f;
     private static bool looseFuel = false;
 
     public Image fuelIndicator;
--- Assets/Karting/Scripts/Video/VideoManager.cs
+++ Assets/Karting/Scripts/Video/VideoManager.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class VideoManager : MonoBehaviour
 {
 
-    UnityEngine.Video.VideoPlayer m_VideoPlayer;
+    public VideoClip[] videoClips;
+    VideoClip videoToPlay;
+    VideoPlayer m_VideoPlayer;
     public RawImage videoImage;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_VideoPlayer = FindObjectOfType<UnityEngine.Video.VideoPlayer>();
-		DebugUtility.HandleErrorIfNullFindObject<UnityEngine.Video.VideoPlayer, VideoManager>(m_VideoPlayer, this);
+        m_VideoPlayer = FindObjectOfType<VideoPlayer>();
+		DebugUtility.HandleErrorIfNullFindObject<VideoPlayer, VideoManager>(m_VideoPlayer, this);
 
         m_VideoPlayer.loopPointReached += IsOver;
         m_VideoPlayer.playOnAwake = false;
@@ -28,11 +31,22 @@ public class VideoManager : MonoBehaviour
 
     // The Video must be named after the animal.
     public void PlayVideo(string _animalName) {
-        m_VideoPlayer.url = "Assets/Karting/Video/" + _animalName + ".mp4";
+        videoToPlay = null;
+        for(int i = 0; i < videoClips.Length; i++) {
+            if(videoClips[i].name.ToString() == _animalName) {
+                videoToPlay = videoClips[i];
+                break;
+            }
+        }
+        if(!videoToPlay) {
+            Debug.Log("There is no Video to play");
+        }
+        m_VideoPlayer.clip = videoToPlay;
+        //m_VideoPlayer.url = "Assets/Karting/Video/" + _animalName + ".mp4";
         PrepareVideo();
     }
 
-    void IsOver(UnityEngine.Video.VideoPlayer vid) {
+    void IsOver(VideoPlayer vid) {
         Debug.Log("Is Over!");
         videoImage.enabled = false;
     }
